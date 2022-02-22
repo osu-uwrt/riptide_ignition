eeae9fd9e29fba2dd6aad6422632c4b1497ee8a6
BTK203 <brach2003@gmail.com>
2022-02-22 00:26:33 -0500

    tempest no longer flies, and added full sim launch file.

--- launch/simulator.launch.py
+++ launch/simulator.launch.py
@@ -13,6 +13,8 @@ import xacro
 def evaluate_xacro(context, *args, **kwargs):
     robot = LC('robot').perform(context)
     debug = LC('debug').perform(context)
+    world = "lake"
+    # world = LC('world').perform(context) uncomment this line if the world is not named "lake"
     x = LC('x').perform(context)
     y = LC('y').perform(context)
     z = LC('z').perform(context)
@@ -42,7 +44,7 @@ def evaluate_xacro(context, *args, **kwargs):
 
     # URDF spawner
     urdf_spawner = ExecuteProcess(
-        cmd=["ign", "service", "-s", "/world/lake/create", "--reqtype", "ignition.msgs.EntityFactory", "--reptype", "ignition.msgs.Boolean", "--timeout", "1000", "--req", "'sdf_filename:", "\"/tmp/{}.xml\",".format(robot), "name:", "\"{}\"'".format(robot)],
+        cmd=["ign", "service", "-s", "/world/{}/create".format(world), "--reqtype", "ignition.msgs.EntityFactory", "--reptype", "ignition.msgs.Boolean", "--timeout", "5000", "--req", "'sdf_filename:", "\"/tmp/{}.xml\",".format(robot), "name:", "\"{}\"'".format(robot)],
         output='both',
         shell=True
     )
@@ -58,8 +60,6 @@ def evaluate_xacro(context, *args, **kwargs):
         output = 'screen',
         parameters=[{'robot_description': xacroData}], # Use subst here
     )
-    
-    
 
     # Message to tf
     message_to_tf_launch = os.path.join(
@@ -86,9 +86,23 @@ def evaluate_xacro(context, *args, **kwargs):
         ]),
         message_to_tf_launch
     ]
+    
+    
+def start_ignition(context, *args, **kwargs):
+    world = LC('world').perform(context) #IF WORLD IS NOT NAMED LAKE, UNCOMMENT THIS LINE AND IT SHOULD (HOPEFULLY) WORK
+    riptide_ignition2_dir = get_package_share_directory("riptide_ignition2")
+    world_path_full = os.path.join(riptide_ignition2_dir, "worlds", world)
+    print("Using world file " + world_path_full + ".world")
+    
+    start_ignition = ExecuteProcess(
+        cmd = ['ign', 'gazebo', '{}.world'.format(world_path_full)],
+        output='both',
+        shell=True
+    )
+    
+    return [ start_ignition ]
 
 def generate_launch_description():
-
     # Message to tf
     sensor_remap = os.path.join(
         get_package_share_directory('riptide_gazebo2'),
@@ -99,12 +113,14 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('robot', default_value='tempest', description='name of the robot to spawn'),
         DeclareLaunchArgument('debug', default_value='0', description='whether to put gazebo into debug mode'),
+        DeclareLaunchArgument('world', default_value='lake', description='The name of the world to launch'),
         DeclareLaunchArgument('x', default_value='0.0', description="X coordinate of the vehicle's initial position (in ENU)"),
         DeclareLaunchArgument('y', default_value='0.0',  description="Y coordinate of the vehicle's initial position (in ENU)"),
         DeclareLaunchArgument('z', default_value='0.0',  description="Z coordinate of the vehicle's initial position (in ENU)"),
         DeclareLaunchArgument('roll', default_value='0.0', description="Z coordinate of the vehicle's initial position (in ENU)"),
         DeclareLaunchArgument('pitch', default_value='0.0', description="Z coordinate of the vehicle's initial position (in ENU)"),
         DeclareLaunchArgument('yaw', default_value='0.0', description="Z coordinate of the vehicle's initial position (in ENU)"),
+        OpaqueFunction(function=start_ignition),
         OpaqueFunction(function=evaluate_xacro),
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(sensor_remap),
